7a4966ea36c4dd68693653b4e8a0d6bb1a48bc8a
Edhar <avuzia@gmail.com>
2021-04-24 10:14:01 +0300

    Tired can't message anything

---
+++ src/component/pages/lecturer/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Jumbotron } from 'reactstrap';
+
+// interface Props {
+//
+// }
+
+export default () => (
+  <Jumbotron>
+    <h1>Managing app settings for lecturer</h1>
+  </Jumbotron>
+);
