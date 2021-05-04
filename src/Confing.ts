8f0110001c5e171f1038997ba6d610ebd981e4b8
Edhar <avuzia@gmail.com>
2021-05-04 16:26:55 +0300

    For heroku

--- src/Confing.ts
+++ src/Confing.ts
@@ -1,6 +1,6 @@
 const config = {
   appId: '7b04f22c-3217-4611-addd-79562e5c6488',
-  redirectUri: 'http://localhost:3000',
+  redirectUri: process.env.AUTH_REDIRECT_URI || 'http://localhost:3000',
   scopes: [
     'user.read',
     'mailboxsettings.read',
